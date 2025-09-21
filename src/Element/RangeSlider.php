74a3975dc9695c057053fbdd3389ae4c64e2dbae
Sang Lostrie <4393-baikho@users.noreply.drupalcode.org>
2025-09-21 19:20:28 +0000

    [#3294240] fix: Field does not support AJAX
    By: baikho
    By: majid.ali

--- src/Element/RangeSlider.php
+++ src/Element/RangeSlider.php
@@ -40,7 +40,8 @@ class RangeSlider extends Range {
   public function getInfo() {
     return [
       '#process' => [
-        [get_class($this), 'processRangeSlider'],
+        [static::class, 'processRangeSlider'],
+        [static::class, 'processAjaxForm'],
       ],
       '#data-orientation' => 'horizontal',
       '#output' => FALSE,
